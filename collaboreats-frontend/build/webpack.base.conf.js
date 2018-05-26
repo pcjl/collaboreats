bb00b99402668d5e3526fd8e5464394d98fd9a0f
Paul Janiszewski <pauljaniszewski44@gmail.com>
2018-05-26 02:09:09 -0400

    JQuery

--- collaboreats-frontend/build/webpack.base.conf.js
+++ collaboreats-frontend/build/webpack.base.conf.js
@@ -3,7 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
-
+const webpack = require('webpack')
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
@@ -11,6 +11,14 @@ function resolve (dir) {
 
 
 module.exports = {
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jquery: 'jquery',
+      'window.jQuery': 'jquery',
+      jQuery: 'jquery'
+    })
+  ]
   context: path.resolve(__dirname, '../'),
   entry: {
     app: './src/main.js'
